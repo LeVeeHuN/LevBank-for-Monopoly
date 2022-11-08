1434f087f8618b77bdcb0bf780bd6bea5a21ecfa
LeVeeHuN <53141474+LeVeeHuN@users.noreply.github.com>
2022-11-08 07:28:47 +0100

    Update tranzakciok.py

--- tranzakciok.py
+++ tranzakciok.py
@@ -4,3 +4,7 @@ class transaction:
         self.kitol = kitol
         self.kinek = kinek
         self.osszeg = osszeg
+
+
+    def getTransaction(self):
+        return [self.mode, self.kitol, self.kinek, self.osszeg]
